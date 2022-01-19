ea03becc4152fab334e1f6933b0b5d42c0177793
krxhna <m.krishnakanth1999@gmail.com>
2022-01-19 09:57:33 +0800

    soemthing2543954

--- src/main.js
+++ src/main.js
@@ -50,18 +50,6 @@ Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBHfHqofD-08feKLT-0SJW3hftXlwsGk_g",
-//   authDomain: "greenlines-69a5c.firebaseapp.com",
-//   projectId: "greenlines-69a5c",
-//   storageBucket: "greenlines-69a5c.appspot.com",
-//   messagingSenderId: "965232689144",
-//   appId: "1:965232689144:web:a4548cafffb8db013c200d",
-//   measurementId: "G-VK1P69RQ7J"
-// // };
-
-// firebase.initializeApp(firebaseConfig);
-// export const db = firebase.firestore();
 
 new Vue({
   router,
