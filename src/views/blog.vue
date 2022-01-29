6f10cba07d497e57f6eb03e215966917e2105c09
krxhna <m.krishnakanth1999@gmail.com>
2022-01-30 03:51:04 +0800

    blog fix2132432

--- src/views/blog.vue
+++ src/views/blog.vue
@@ -24,7 +24,20 @@
 
     <div class="third">
       <div class="cards">
-        vard
+        <div class="img">
+           <img
+           class="img"
+              src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df" >
+        </div>
+      <div class="date">
+        23/4/1232
+      </div>
+        <div class="card_title">
+          Tab is not working for some reason
+        </div>
+        <div class="card_des">
+          fdsadf ehat ever this is i like typing shit liek this i dk what ithis is it maybe this gonna be fo rseo or soemhting i ofnt liek tht epelase do need to cpomplicate the this this i am yours
+        </div>
       </div>
       <div class="cards">
         vard
@@ -55,7 +68,7 @@ export default {
 
 .first {
   /* background-color: aqua; */
-  width: 100vw;
+  width: 98vw;
   height: 10vh;
   text-align: center;
   color: black;
@@ -67,6 +80,13 @@ export default {
 .cont{
   background-color: white;
   width: 34vw;
+}
+
+
+.card_title{
+  font-size: 2rem;
+  font-weight: 900;
+  font-family: poppins;
 }
 
 .secpanel{
@@ -84,33 +104,53 @@ export default {
 
 
 .third{
+ 
+
   grid-gap: 2rem;
   overflow: hidden;
  display:grid;
  padding-left: 8vw;
- padding-left: 8vw;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-auto-flow: row;
+padding-right: 8vw;
+padding-top: 5vh;
+ grid-template-columns: 1fr 1fr 1fr;
+  grid-auto-flow: row; 
 }
 
-.content{
+.content{ 
   font-weight: 300;
   font-size: 1.4rem;
 }
 
 
-.cards{
+.img{
+  background-color: blue;
+   object-fit: cover;
+
+  height: 20rem;
+  
+  width: 100%;
+}
+
+
+
+.carfsadds{
+      --tw-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1);
+    --tw-shadow-colored: 0 20px 25px -5px var(--tw-shadow-color), 0 8px 10px -6px var(--tw-shadow-color);
+    box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
   background-color: aqua;
-  width: 15rem;
-  height: 15rem;
+  width: 30rem;
+  display: flex;
+  flex-direction: column;
+  height: 40rem;
 }
 
 .sec {
+
    padding-left: 10vw;
   display: flex;
   flex-direction: row;
   /* background-color: rgb(81, 180, 139); */
-  width: 100vw;
+  width: 98vw;
   height: 50vh;
   margin-top: 3rem;
   text-align: center;
