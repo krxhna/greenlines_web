69aa0bc0e4b1411e97bcbaa29286e2b83ffd069d
krxhna <m.krishnakanth1999@gmail.com>
2022-02-07 22:14:05 +0800

    alpha landing page on pgress

---
+++ src/views/alpha.vue
@@ -0,0 +1,126 @@
+<template>
+ <div class="alpha">
+     <div class="navbar_alpha" style="display:flex;">
+         <div class="a">greenlines</div>
+         <div class="a">greenlines</div>
+         <div class="a">greenlines</div>
+         <div class="a">greenlines</div>
+         
+     </div>
+
+     <div class="em">
+         
+     </div>
+     <div class="middle_alpha">
+
+         <div class="slogan_alpha">
+            <div class="alp_text">
+                 <div class="head_alp">Do Valaution</div>
+             <div class="colortext"> Faster.</div>
+             <div class="textalp">
+                your valuation workspace do everything 
+from checking sec reports, to industry 
+averages to amcro data all in one place
+            </div>
+            <div class="buttonsometh">
+                <button class="btn_alp">Get Started</button>
+            </div>
+            </div>
+            
+
+         </div>
+     </div>
+     </div> 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style>
+
+.alpha{
+    background-color: black;
+    width: 100vw;
+    height: 100vh;
+    text-align: center;
+    color: white;
+    /* font-size: 7rem;
+    font-weight: 900; */
+    font-family: poppins;
+}
+
+.em{
+    height: 10vh;
+}
+
+.middle_alpha{
+    margin-left: 10vh;
+    width: 80vw;
+    height: 60vh;
+    background-color: transparent;
+}
+
+.slogan_alpha{
+    background-color: #000000;
+    
+    
+    display: flex;
+    justify-content: flex-start;
+}
+
+.textalp{
+    text-align: start;
+    width: 40vw;
+    font-family: Roboto;
+font-style: normal;
+font-weight: normal;
+font-size: 1.5rem;
+
+
+}
+
+.buttonsometh{
+    display: flex;
+    justify-content: flex-start;
+    margin-top: 3vh;
+
+    background-color: rgb(0, 0, 0);
+    width: 45vw;
+   place-content: flex-start;
+}
+
+.btn_alp{
+    
+   
+    border: none;
+    font-size: 1.5rem;
+    color:black;
+    font-family: Roboto;
+    padding: 10px;
+    background: #C4C4C4;
+border-radius: 0.1rem;
+    margin-top: 10px;
+}
+
+
+.head_alp{
+    text-align: start;
+    font-size: 5rem;
+    font-weight: 900;
+    font-family: poppins;
+}
+
+.colortext{
+    text-align: start;
+    font-size: 5rem;
+    font-weight: 900;
+    font-family: poppins;
+}
+
+
+
+</style>
