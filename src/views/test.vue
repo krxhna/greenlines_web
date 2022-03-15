9a3e657c8155a35e0e461d829a111df502908678
krxhna <m.krishnakanth1999@gmail.com>
2022-03-16 03:21:28 +0530

    testing new home page in test vue

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,20 +1,119 @@
-.<template>
-  <div class="testdsad">
-    blogblog
-   <div class="div">
-     <iframe src='https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=MEOW&type=10-K&dateb=&owner=exclude&count=40#contentDiv' width='100%' height='100%'> </iframe>
+<template>
+<div class="homesearch">
+  <div class="home_nav">
+
+   <div class="logoholder" style="background-color: transparent;">
+      <img src="../assets/shortlogo.png" style="height: 10vh; margin: 1rem;"/>
    </div>
+      <div class="search_home">
+       
+        
+      </div>
+
+
 
   </div>
+</div>
 </template>
 
 <script>
-export default {
-  
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-}
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+  },
+
+  methods: {
+
+
+    new() {
+      this.$router.push("/new");
+    },
+
+    handleClick(ticker) {
+      if (this.isMobile()) {
+        this.$router.push({
+          path: "go/" + ticker,
+        }),
+          this.clicked();
+      } else {
+        this.$router.push({
+          path: "dashboard/" + ticker,
+        }),
+          this.clicked();
+      }
+    },
+
+
+      the_go: function (ticker) {
+      this.ticker_list.push(ticker);
+      this.collect_tickers(ticker);
+      this.popup = false;
+      this.international = false;
+      // this.$router.push({
+      //   path: "/dashboard/" + ticker,
+      // });
+
+      // this.$forceUpdate();
+      // location.reload();
+      // this.$router.go(this.$router.currentRoute);
+    },
+    isMobile() {
+      if (screen.width <= 760) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    login() {
+      this.$gtag.event("login", { method: "Google" });
+    },
+    clicked() {
+      this.$gtag.event("clicked search", { method: "Google" });
+    },
+    clickedback() {
+      this.$gtag.event("clicked back", { method: "Google" });
+    },
+  },
+
+  //  mounted() {
+  //     window.addEventListener("keypress", function(enter) {
+  //       console.log(String.fromCharCode(enter.keyCode));
+  //       alert(String.fromCharCode(enter.keyCode));
+  //       // handleClick(message.toUpperCase());
+  //     }.bind(this));
+  //   }
+};
 </script>
 
-<style>
+<style scoped>
+
+.homesearch{
+  display: flex;
+  flex-direction: column;
+  background-color: #000000;
+  height: 100vh;
+  background: linear-gradient(180deg, #000000 50.01%, #0C5718 86.37%);
+}
+
+.home_nav{
+ 
+  align-items: center;
+  justify-items: center;
+  margin: 3vw;
+    display: flex;
+  flex-direction: row;
+
+}
+
+.search_home{
+ height: 7vh;
+ border-radius: 0px 7px;
+ width: 50vh;
+background: linear-gradient(147.36deg, rgba(255, 255, 255, 0.49) -6.58%, rgba(196, 196, 196, 0.39) 100%);
+}
 
 </style>
