d6f39f26c673b01f2ac27d41c6dc839e71cd4b68
krxhna <m.krishnakanth1999@gmail.com>
2022-04-03 19:20:35 +0800

    workspace navbar

--- src/views/workspace.vue
+++ src/views/workspace.vue
@@ -1,7 +1,117 @@
 <template>
-  <div class="container mt-5">
-      {{user}}
-      {{name}}
+  <div class="flex"> 
+    <div class="navbar">
+      <ul>
+        <li class="navbarnav">
+          <router-link :to="{ path: '/' }" class="thing" replace>
+            ←Back</router-link
+          >
+          <div class="searchbox">
+            <input
+              :key="$route.fullPath"
+              type="text"
+              class="search-bar1"
+              :placeholder="'$' + ticker"
+              @keyup.enter="the_go(message.toUpperCase())"
+              v-model="message"
+            />
+          </div>
+
+          <div class="buttondiv">
+            <div class="a" style="color: white"></div>
+            <button
+              :key="$route.fullPath"
+              class="gobutton"
+              @click="the_go(message.toUpperCase())"
+            >
+              GO
+            </button>
+          </div>
+          <div class="rectangle"></div>
+          <h5
+            v-if="user == null"
+            style="color: green; text-transform: capitalize"
+          >
+            login/signup for free to get acess
+          </h5>
+          <h5 v-else style="color: green; text-transform: capitalize"></h5>
+
+          <router-link
+            :to="{ path: '/dashboard/' + ticker }"
+            class="thing"
+            replace
+            >Dashboard</router-link
+          >
+          <router-link
+            :to="{ path: '/industry/' + ticker }"
+            class="thing"
+            replace
+            >Industry</router-link
+          >
+                    <router-link
+            :to="{ path: '/macro/' + ticker }"
+            class="thing"
+            replace
+            >macro</router-link
+          >
+          <div class="current">workspace</div>
+          <router-link
+            :to="{ path: '/funds/' + ticker }"
+            class="thing"
+            replace
+            >Funds</router-link
+          >
+          <h7 class="thing">News( coming soon)</h7>
+          <!-- <h7 class="thing">Macroview( coming soon)</h7> -->
+
+          <h1 v-if="user == null">
+            <b-button
+              @click="googlesigin"
+              class="btn btn-success"
+              style="color: black"
+              >Login/signinup</b-button
+            >
+          </h1>
+
+          <h1>
+            <a style="color: #0f0f0f">,</a>
+            <div class="fg" style="display: flex; flex-direction: column">
+              <b-button
+                @click="openstripe()"
+                class="btn btn-success"
+                style="color: black; background-color: #38ea41"
+                >GO pro <i class="fas fa-crown"></i
+              ></b-button>
+              <b-button
+                @click="logout"
+                class="btn btn-success"
+                style="color: gray; background-color: #0f0f0f; border: none"
+                >logout</b-button
+              >
+            </div>
+          </h1>
+          <div class="footernotes" style="display:flex; ">
+            <a
+              style="color: white"
+              href="https://mail.google.com/mail/?view=cm&fs=1&to=krish@greenlines.club"
+              target="_blank"
+              >📧contact</a
+            >
+            <a
+              style="color: white"
+              href="https://mail.google.com/mail/?view=cm&fs=1&to=krish@greenlines.club"
+              target="_blank"
+              >🐞Report a bug</a
+            >
+          </div>
+        </li>
+      </ul>
+    </div>
+
+    <div class="container mt-5">
+      {{user.email}}
+      {{email}}
+      {{collectiondata}}
     
     <div class="row">
       <div class="col form-inline">
@@ -97,6 +207,7 @@
       </div>
     </div>
   </div>
+  </div>
 </template>
 
 <script>
@@ -114,6 +225,8 @@ export default {
 
   mounted(){
       console.log(this.arrBackLog);
+      this.email = firebase.auth().currentUser.email;
+
 
   },
 
@@ -133,12 +246,13 @@ export default {
   },
     firestore() {
     return {
-
-      name: db.doc("users/katkat@gmail.com").onSnapshot((doc) => {
-        this.name = doc.data();
-        this.arrBackLog = doc.data().tickers;
-        this.email = doc.data().email;
+      
+      collectiondata: db.doc("users/"+this.email).onSnapshot((doc) => {
+        this.collectiondata = doc.data();
       }),
+  
+
+      
     };
   },
 
@@ -146,6 +260,7 @@ export default {
 created() {
     firebase.auth().onAuthStateChanged((user) => {
       this.user = user;
+
     });
   },
 
@@ -156,6 +271,7 @@ created() {
       user:"dfs",
       name:"dsf",
       email:"",
+      collectiondata:"",
       // 4 arrays to keep track of our 4 statuses
       arrBackLog: [
         { name: "Code Sign Up Page" },
