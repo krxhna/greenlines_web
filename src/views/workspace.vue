749a4d842cdf7ed06b0f8d2c039f1a3e5f3a396a
krxhna <m.krishnakanth1999@gmail.com>
2022-04-04 15:16:21 +0800

    workspace ui update

--- src/views/workspace.vue
+++ src/views/workspace.vue
@@ -103,7 +103,7 @@
       </ul>
     </div>
 
-    <div class="container pt-5 text-danger" style="background-color: #212121;">
+    <div class="container pt-5 text-danger" style="background-color: white;">
       <!-- {{user.email}} -->
       
 
@@ -154,11 +154,13 @@
             group="tasks"
           >
             <div
-              class="list-group-item"
+              class="list-group-item m-1 rounded shadow-sm" style="background-color: #212121"
               v-for="element in arrInProgress"
               :key="element"
-            >
-              {{ element}}
+            >  <div class="text-3xl text-light">{{ element.toUpperCase() }}</div>
+              <button class=" text-sm p-1 rounded" style="color:gray" >dashboard <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+</svg></button>
             </div>
           </draggable>
         </div>
@@ -176,11 +178,14 @@
             group="tasks"
           >
             <div
-              class="list-group-item"
+              class="list-group-item m-1 rounded shadow-sm" style="background-color: #212121"
               v-for="element in arrDone"
               :key="element.name"
             >
-              {{ element }}
+               <div class="text-3xl text-light">{{ element.toUpperCase() }}</div>
+              <button class=" text-sm p-1 rounded" style="color:gray" >dashboard <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+</svg></button>
             </div>
           </draggable>
         </div>
